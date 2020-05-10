deb7192d79541a0b3f5264540a6892815b5e7702
PepegaRonalds <58549429+PepegaRonalds@users.noreply.github.com>
2020-05-10 13:25:41 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
 
     $("#header").css("background", "image");
     $("#header").css("width", "100%");
-    $("#header").css("height", "30%");
+    $("#header").css("height", "20%");
 
-    $("#body").css("background", "black");
+    $("#body").css("background", "#1e1d1d");
     $("#body").css("width", "100%");
     $("#body").css("height", "75%");
     
@@ -19,7 +19,7 @@ $(document).ready(function() {
     $("#navigation_header").css("padding-top", "20px");
     $("#navigation_header").css("padding-bottom", "20px");
 
-    $("#body_content").css("background", "black");
+    $("#body_content").css("background", "#1e1d1d");
 
     $("#personal_photo").css("height", "250px");
     $("#personal_photo").css("width", "250px");
@@ -28,7 +28,49 @@ $(document).ready(function() {
     $(".magic-image").css("height", "350px")
     $(".magic-image").css("width", "350px")
 
-    $("#image_content").css("padding-top", "100px");
+    $("#image_content").css("padding-top", "120px");
     $("#form_content").css("padding-top", "100px");
 
+    $(".#button").html("navigation_buttons","Par_Mani");
+    $(".#button").html(".navigation_buttons","Mani_darbi");
+    $(".#button").html("navigation_buttons","Kontakti")
+
+    $(document).ready(function() {
+        var settings = {
+            "bodyContainerDisplay":"block",
+            "imageContainerDisplay":"none",
+            "formContainerDisplay":"none",
+        };
+            $("body_container").css("display", settings.bodyContainerDisplay);
+            $("image_container").css("display", settings.imageContainerDisplay);
+            $("form_content").css("display", settings.formContainerDisplay);
+    }
+
+    $("#navigation_buttons p").click(function(){
+        $(this).css("color"" #f99e70 ");
+
+        var text = $(this).text();
+
+        if(text == "Par mani") {
+            $("#body_container").css("display","block");
+            $("#body_container").css("display","none");
+            $("#form_content").css("display","none");
+            $("#my_works").css("color","#f99e70");
+            $("#info").css("color","#f99e70")
+        }
+        if(text == "Mani darbi") {
+            $("#body_container").css("display","none");
+            $("#body_container").css("display","block");
+            $("#form_content").css("display","none");
+            $("#my_works").css("color","#f99e70");
+            $("#info").css("color","#f99e70")
+        }
+        if(text == "Kontakti") {
+            $("#body_container").css("display","none");
+            $("#body_container").css("display","none");
+            $("#form_content").css("display","block");
+            $("#my_works").css("color","#f99e70");
+            $("#info").css("color","#f99e70")
+        }
+    }
 });
